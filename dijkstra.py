6a11fd58c651d33ae07c7dc1f0455fa9a9dae234
TallesH13 <160864378+TallesH13@users.noreply.github.com>
2025-08-05 09:45:43 -0300

    dijkstra.py

--- dijkstra.py
+++ dijkstra.py
@@ -3,25 +3,25 @@ def dijkstra(grafo, origem):
     dist = {}
     prev = {}
 
-    for v in grafo:
+    for v in range(grafo.numVertices):
         dist[v] = math.inf
         prev[v] = None
     
     dist[origem] = 0
     prev[origem] = origem
 
-    O = set(grafo.keys()) # vertices principais listados na lista de adjacencias dos vertices
+    O = set(range(grafo.numVertices)) # vertices principais listados na lista de adjacencias dos vertices
     C = set() # c sao os vertices os quais todos vizinhos foram visitados
 
     def acha_distancia(x): #funcao pra achar menor distancia pra um vertice x
         return dist[x] 
     
-    while C != set(grafo.keys()):
+    while C != set(range(grafo.numVertices)):
         u = min(O, key=acha_distancia) # procura a menor distancia dentre os vertices ainda nao explorados
-        C = C + {u}
+        C = C | {u}
         O = O - {u}
 
-        for v, peso in grafo[u]:
+        for v, peso in grafo.vizinhos(u):
             if v not in C:
                 if (dist[v] > (dist[u] + peso)):
                     dist[v] = dist[u] + peso
